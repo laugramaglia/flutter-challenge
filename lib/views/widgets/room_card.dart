748633baf894654f7c706c3ee0e4984458195a6f
Isaac <dev.iceztech@gmail.com>
2023-11-01 16:45:55 +0100

    home layout almost complete.

--- lib/views/widgets/room_card.dart
+++ lib/views/widgets/room_card.dart
@@ -3,16 +3,7 @@ import 'package:flutter/material.dart';
 import '../../constants/app_colors.dart';
 
 class RoomCard extends StatelessWidget {
-  const RoomCard(
-      {super.key,
-      required this.bgColor,
-      required this.textColor,
-      required this.title,
-      required this.subTitle,
-      required this.isActive,
-      required this.icon});
-  final Color bgColor;
-  final Color textColor;
+  const RoomCard({super.key, required this.title, required this.subTitle, required this.isActive, required this.icon});
   final String title;
   final String subTitle;
   final bool isActive;
@@ -21,31 +12,39 @@ class RoomCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: isActive ? primaryKColor : whiteKColor,
+      elevation: 0,
       child: Padding(
-        padding: const EdgeInsets.all(8),
+        padding: const EdgeInsets.all(12),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
               width: 32.0,
               height: 32.0,
-              color: deactivateColor,
+              decoration: BoxDecoration(
+                color: isActive ? whiteKColor.withOpacity(0.16) : Colors.black.withOpacity(0.16),
+                borderRadius: BorderRadius.circular(4),
+              ),
               child: Icon(
                 icon,
-                color: textColor,
+                color: isActive ? whiteKColor : primaryKColor,
               ),
             ),
+            const SizedBox(height: 8),
             Text(
               title,
               style: TextStyle(
-                color: textColor,
+                color: isActive ? whiteKColor : defaultTextKColor,
                 fontWeight: FontWeight.w700,
                 fontSize: 16.0,
               ),
             ),
+            const SizedBox(height: 4),
             Text(
               subTitle,
               style: TextStyle(
-                color: bgColor == primaryKColor ? textLightKColor : whiteKColor,
+                color: isActive ? whiteKColor : textLightKColor,
                 fontSize: 12.0,
                 fontWeight: FontWeight.w500,
               ),

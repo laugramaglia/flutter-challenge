bfadde15181ce6a964dfc26ef757409ae8e2ecd4
Laureano Gramaglia <>
2023-11-30 21:40:21 -0300

    set current page to analitycs

--- lib/features/presentation/ui/pages/rooms_view.dart
+++ lib/features/presentation/ui/pages/rooms_view.dart
@@ -1,29 +1,16 @@
 import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_challange/comon/async_state.dart';
-import 'package:flutter_challange/comon/async_state_notifier.dart';
-import 'package:flutter_challange/features/data/models/device.dart';
-import 'package:flutter_challange/features/domain/repositories/device_repository.dart';
 import 'package:flutter_challange/features/presentation/providers/device_provider.dart';
 import 'package:flutter_challange/features/presentation/ui/widgets/device_card_small.dart';
 import 'package:provider/provider.dart';
-
-import '../widgets/bottom_navigation.dart';
 
 class RoomsView extends StatelessWidget {
   const RoomsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Challenge App'),
-        ),
-        body: _RoomState(),
-        bottomNavigationBar: BottomNavigationBarWidget(initialIndex: 0),
-    );
+    return const _RoomState();
   }
 }
 
--- lib/features/presentation/ui/pages/single_room.dart
+++ lib/features/presentation/ui/pages/single_room.dart
@@ -1,21 +1,13 @@
 import 'package:flutter/material.dart';
 
-import '../widgets/bottom_navigation.dart';
-
 class SingleRoomView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Single Room View'),
+    return Center(
+      child: Text(
+        'Single Room Placeholder',
+        style: TextStyle(fontSize: 24),
       ),
-      body: Center(
-        child: Text(
-          'Single Room Placeholder',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBarWidget(initialIndex: 1)
     );
   }
 }
